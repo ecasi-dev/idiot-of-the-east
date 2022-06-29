794f53a1654552fc9c24ee9eca96a5f17954c50f
ecasi-dev <emiliano.casi@internetbrands.com>
2022-06-29 20:10:43 +0800

    Fix website title

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,7 +4,7 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'idiot-of-the-east',
+    title: 'Idiot Of The East',
     htmlAttrs: {
       lang: 'en',
     },
